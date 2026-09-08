9e5e2411866e9e508f9c843edf2d6ca8ef826a01
monusaini173 <monusaini1743@gmail.com>
2026-09-08 11:35:43 +0530

    Remove unnecessary blank line before the About section in index.html

--- index.js
+++ index.js
@@ -4,29 +4,25 @@ const menuBtn = document.getElementById("menuBtn");
 const nav = document.querySelector("nav");
 
 menuBtn.addEventListener("click", function () {
-    nav.classList.toggle("active");
+  nav.classList.toggle("active");
 });
-
 
 // Button
 
 function showMessage() {
-    alert("Welcome! Let's get started.");
+  alert("Welcome! Let's get started.");
 }
-
 
 // Contact Form
 
 const form = document.getElementById("contactForm");
 
 form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    event.preventDefault();
+  const name = document.getElementById("name").value;
 
-    const name = document.getElementById("name").value;
+  alert("Thank you " + name + "! Your message has been received.");
 
-    alert("Thank you " + name + "! Your message has been received.");
-
-    form.reset();
-
+  form.reset();
 });
